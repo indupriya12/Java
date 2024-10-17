6b10245aca3bef87e29fbb2fd82109a5ecabeefb
indupriya12 <151903467+indupriya12@users.noreply.github.com>
2024-10-17 09:40:20 +0530

    Update Arraydemo.java

--- Arraydemo.java
+++ Arraydemo.java
@@ -1,20 +1,17 @@
+import java.io.*;
 import java.util.*;
-class Main
-{
-    public static void main (String[] args) {
+public class ArrayDemo{
+    public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
-        System.out.println("Enter size of the array:");
+        System.out.println("Enter no.of elements of array: ");
         int n=sc.nextInt();
         int arr[]=new int[n];
-        System.out.print("Enter "+n+"values into the array:");
-        for(int i=0;i<n;i++)
-        {
+        System.out.println("Enter "+n+" values");
+        for(int i=0;i<n;i++){
             arr[i]=sc.nextInt();
         }
-        System.out.print("the elements of the array are:");
+        System.out.println("The values in the array are");
         for(int i=0;i<n;i++)
-        {
-            System.out.println(arr[i]+"");
-        }
+        System.out.println(arr[i]+" ");
     }
 }
